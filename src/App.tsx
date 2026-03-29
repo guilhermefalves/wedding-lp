4cc8b8f1af2ab7e6cef670665fc0ba699399093c
Guilherme Alves <guihalves20@gmail.com>
2026-03-29 12:51:58 -0300

    add vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Home from "./pages/Home";
 import Convite from "./pages/Convite";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <SpeedInsights />
       <Routes>
         <Route path="/convite" element={<Convite />} />
         <Route path="*" element={<Home />} />
